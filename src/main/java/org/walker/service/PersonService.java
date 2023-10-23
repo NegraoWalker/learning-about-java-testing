35b1c20c8f60de5af6c6922d3645e12220455232
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-10-23 13:41:06 -0300

    Implementado um teste para validar exceptions

--- src/main/java/org/walker/service/PersonService.java
+++ src/main/java/org/walker/service/PersonService.java
@@ -2,9 +2,17 @@ package org.walker.service;
 
 import org.walker.model.Person;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class PersonService implements PersonServiceInterface {
     @Override
     public Person createPerson(Person person) {
+
+        if (person.getEmail() == null || person.getEmail().isBlank()) {
+            throw new IllegalArgumentException("The Person e-Mail is null or empty!");
+        }
+        var id = new AtomicLong().incrementAndGet();
+        person.setId(id);
         return person;
     }
 }
--- src/test/java/org/walker/person/PersonServiceTest.java
+++ src/test/java/org/walker/person/PersonServiceTest.java
@@ -7,6 +7,8 @@ import org.walker.model.Person;
 import org.walker.service.PersonService;
 import org.walker.service.PersonServiceInterface;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PersonServiceTest {
     @DisplayName("When Create a Person with Success Should Return a Person Object") //Quando criar uma pessoa com sucesso deve retornar um objeto Person
     @Test
@@ -29,6 +31,39 @@ public class PersonServiceTest {
         // When / Act:
         Person actual = service.createPerson(person);
         // Then / Assert:
-        Assertions.assertEquals(person.getFirstName(), actual.getFirstName(), () -> "The FistName is Different!");
+        assertEquals(person.getFirstName(), actual.getFirstName(), () -> "The FistName is Different!");
+    }
+
+    @DisplayName("When Create a Person with Success Should Contains Valid Fields in Returned Person Object")
+    @Test
+    void testCreatePerson_When_Success_ShouldContainsValidFieldsInReturnedPersonObject() {
+
+        // Given / Arrange
+        PersonServiceInterface service = new PersonService();
+
+        Person person = new Person("Walker","Negrão","Londrina - PR","Masculino","walker@gmail.com");
+        // When / Act:
+        Person actual = service.createPerson(person);
+
+        // Then / Assert
+        assertNotNull(person.getId(),()-> "Person ID is missing!");
+        assertEquals(person.getFirstName(), actual.getFirstName(), () -> "The Person FistName is Incorrect!");
+        assertEquals(person.getLastName(), actual.getLastName(), () -> "The Person LastName is Incorrect!");
+        assertEquals(person.getAddress(), actual.getAddress(), () -> "The Person Address is Incorrect!");
+        assertEquals(person.getGender(), actual.getGender(), () -> "The Person Gender is Incorrect!");
+        assertEquals(person.getEmail(), actual.getEmail(), () -> "The Person Email is Incorrect!");
+    }
+
+    @DisplayName("When Create a Person with null e-Mail Should throw Exception")
+    @Test
+    void testCreatePerson_WhithNullEMail_ShouldThrowIllegalArgumentException() {
+        // Given / Arrange
+        PersonServiceInterface service = new PersonService();
+        Person person = new Person();
+        person.setEmail(null);
+        // When / Act
+        // Then / Assert
+        var exception = assertThrows(IllegalArgumentException.class, () -> service.createPerson(person), () -> "Empty e-Mail should have cause an IllegalArgumentException!");
+        assertEquals("The Person e-Mail is null or empty!",exception.getMessage(),()-> "Exception error message is incorrect!");
     }
 }
